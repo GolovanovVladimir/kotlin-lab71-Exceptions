e194efd9a8a60026758e5fb72410c5588cbdde0e
Vladimir Golovanov2 <golovanov.vladimir.ivanovich@gmail.com>
2023-02-28 22:56:33 +0300

    changing N2

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -9,31 +9,36 @@ class MainKtTest {
     fun wallServiceAddCommentTrue() {
         val post = Post(1,1,1,1,1,"cheking",1,1,1,"lya-lya",true,true,1,null,null)
         var postId = post.id
+//      var postId = 2
         var result = false
         val comment0 = Comment(10,"678")
         WallService.Add(post)
-        result= WallService.findById(postId)
+/*       result= WallService.findById(postId)
         if (result == true)
         {
             WallService.createComment(postId, comment0)
         }
         assertEquals(true, result)
-
+*/
+        result= WallService.findById(postId)
+        assertEquals(true, result)
     }
 
     @Test(expected = PostNotFoundException::class)
     fun shouldThrow() {
         val post = Post(1,1,1,1,1,"cheking",1,1,1,"lya-lya",true,true,1,null,null)
-        var postId = -1
+        var postId = 2
         var result = false
         val comment0 = Comment(10,"678")
         WallService.Add(post)
-        if (WallService.findById(postId) == true){
+/*        if (WallService.findById(postId) == true){
             WallService.createComment(postId, comment0)
         }
         else
         {
             throw PostNotFoundException("No exist post with ${post.id}")
         }
+*/
+        WallService.createComment(postId, comment0)
     }
 }
